f67448015d90efb4e2fa3d90d7f250839392b5fd
Alex10161010 <eliasalejandromome@gmail.com>
2021-05-17 01:59:30 -0500

    mi primer Observable

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,21 @@
+import {Observable} from 'rxjs'
 
 
-console.log('Hola Mundo!');
+const obs$ = new Observable<string>(subscribe=>{
+
+    const nombre: string ='Hola Mundo soy Elias';
+    subscribe.next(nombre);
+    subscribe.next(nombre);
+    subscribe.next(nombre);
+    subscribe.next(nombre);
+    subscribe.complete();
+    subscribe.next(nombre);
+    subscribe.next(nombre);
+});
+
+
+obs$.subscribe(console.log);
+
 
 
 
